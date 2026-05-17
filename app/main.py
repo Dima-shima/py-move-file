fb1bd7380bad9c76b904b17fb23d31ec4a36ea42
Dima-shima <shifrin1970@gmail.com>
2026-05-17 13:31:52 +0300

    implemented move-file

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ import os
 def move_file(command: str) -> None:
     string = list(command.split())
     if len(string) == 3:
-        command_from_string = string[0]
         file_in = string[1]
         file_out = string[2]
     if "/" not in file_out:
@@ -17,7 +16,7 @@ def move_file(command: str) -> None:
             if not os.path.isdir(new_directory):
                 os.mkdir(new_directory)
             path = new_directory + "/"
-            file_out = file_out[(file_out.find("/"))+1:]
+            file_out = file_out[(file_out.find("/")) + 1:]
         with open(file_in, "r") as file1, \
                 open(path + file_out, "w") as file2:
             content = (file1.read())
