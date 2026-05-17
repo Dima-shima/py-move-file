ad0e04e84a296e83f4506d3fa40f85f16b45bdaa
Dima-shima <shifrin1970@gmail.com>
2026-05-17 16:18:26 +0300

    implemented move-file

--- app/main.py
+++ app/main.py
@@ -4,20 +4,21 @@ import os
 def move_file(command: str) -> None:
     string = list(command.split())
     if len(string) != 3:
-        raise "Error arguments"
+        raise ValueError("Error arguments")
     command_from_string, file_in, file_out = string
     if command_from_string == "mv":
         if "/" not in file_out:
-            print("123")
             os.rename(file_in, file_out)
         if "/" in file_out:
             path = ""
             while "/" in file_out:
                 new_directory = file_out[0:(file_out.find("/"))]
                 path = os.path.join(path, new_directory)
-                if not os.path.isdir(new_directory):
+                if not os.path.isdir(path):
                     os.mkdir(path)
                 file_out = file_out[(file_out.find("/")) + 1:]
+            if file_out == ".txt":
+                file_out = file_in
             with open(file_in, "r") as file1, \
                     open(os.path.join(path, file_out), "w") as file2:
                 content = (file1.read())
